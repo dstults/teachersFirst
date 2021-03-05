ac7aed96e244a86bdeadf1d429dcfe08ec21cbb3
Darren Stults <drankof@gmail.com>
2021-03-04 20:51:03 -0800

    DarrenS: Added comment to opening (#85)
    * Added comment on line 16

--- src/main/java/edu/lwtech/csd297/teachersfirst/pojos/Opening.java
+++ src/main/java/edu/lwtech/csd297/teachersfirst/pojos/Opening.java
@@ -4,31 +4,29 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class Opening {
-    private Date startDate;
+	private Date startDate;
 	private Date endDate;
-	
-	private int teacherID;
-    private String teacherName;
-	
-	
-	public Opening(
-			int startYear, int startMonth, int startDay, int startHour, int startMinute, 
-			int endYear, int endMonth, int endDay, int endHour, int endMinute,
-			int teacherID, String teacherName) 
-	{		
-		this.teacherID = teacherID;
-    this.teacherName = teacherName;
-		this.startDate = newDate(startYear,startMonth,startDay,startHour,startMinute);
-		this.endDate = newDate(endYear,endMonth,endDay,endHour,endMinute);
-  }
 
-    public Opening(Date startDate, Date endDate, int teacherID, String teacherName){
-        this.teacherID = teacherID;
-        this.teacherName = teacherName;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
-	
+	private int teacherID;
+	private String teacherName;
+
+	public Opening(int startYear, int startMonth, int startDay, int startHour, int startMinute, int endYear,
+			int endMonth, int endDay, int endHour, int endMinute, int teacherID, String teacherName) {
+
+		// Set initial values
+		this.teacherID = teacherID;
+		this.teacherName = teacherName;
+		this.startDate = newDate(startYear, startMonth, startDay, startHour, startMinute);
+		this.endDate = newDate(endYear, endMonth, endDay, endHour, endMinute);
+	}
+
+	public Opening(Date startDate, Date endDate, int teacherID, String teacherName) {
+		this.teacherID = teacherID;
+		this.teacherName = teacherName;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
 	private static Date newDate(int year, int month, int day, int hour, int minute) {
 		Calendar cal = Calendar.getInstance();
 		cal.setTimeInMillis(0);
@@ -36,22 +34,20 @@ public class Opening {
 		cal.set(year, month, day, hour, minute, 0);
 		return cal.getTime();
 	}
-	
+
 	public Date getStartDate() {
 		return this.startDate;
 	}
-	
+
 	public Date getEndDate() {
 		return this.endDate;
 	}
-	
-    /*
-	public int getTeacherID() {
-		return this.teacherID;
-	}
-    */
 
-    public int getRecID() {
+	/*
+	 * public int getTeacherID() { return this.teacherID; }
+	 */
+
+	public int getRecID() {
 		return this.teacherID;
 	}
 
@@ -65,21 +61,22 @@ public class Opening {
 		this.teacherID = recID;
 	}
 
-    public String getName(){
-        return this.teacherName;
-    }
+	public String getName() {
+		return this.teacherName;
+	}
 
-    public Appointment newAppointment(Date startDate, int duration, int studentID){
-        Appointment newAppointment = new Appointment(startDate, addMinutes(startDate, duration), studentID, this.teacherID);
-        //check other appoints from the appointment dao
-		//approve or disapprove
-        return newAppointment;
-    }
+	public Appointment newAppointment(Date startDate, int duration, int studentID) {
+		Appointment newAppointment = new Appointment(startDate, addMinutes(startDate, duration), studentID,
+				this.teacherID);
+		// check other appoints from the appointment dao
+		// approve or disapprove
+		return newAppointment;
+	}
 
-    public Date addMinutes(Date date, int minutes){
-        int currentMinutes = date.getMinutes();
-        date.setMinutes(currentMinutes + minutes);
-        return date;
-    }
+	public Date addMinutes(Date date, int minutes) {
+		int currentMinutes = date.getMinutes();
+		date.setMinutes(currentMinutes + minutes);
+		return date;
+	}
 
 }
