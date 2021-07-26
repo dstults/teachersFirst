c448adf749bfb5bdbd3629c86d6e70c2210f1998
DranKof <drankof@gmail.com>
2021-07-25 22:04:11 -0700

    DarrenS: Added refund logic to state changer action

--- src/main/java/org/funteachers/teachersfirst/actions/MissOrCompleteAppointmentAction.java
+++ src/main/java/org/funteachers/teachersfirst/actions/MissOrCompleteAppointmentAction.java
@@ -1,24 +1,28 @@
 package org.funteachers.teachersfirst.actions;
 
 import javax.servlet.http.*;
+import javax.ws.rs.NotAcceptableException;
 
 import org.funteachers.teachersfirst.*;
+import org.funteachers.teachersfirst.daos.DAO;
 import org.funteachers.teachersfirst.obj.*;
 
 public class MissOrCompleteAppointmentAction extends ActionRunner {
 
-	String subAction;
-
-	public MissOrCompleteAppointmentAction(HttpServletRequest request, HttpServletResponse response, String subAction) {
+	public MissOrCompleteAppointmentAction(HttpServletRequest request, HttpServletResponse response) {
 		super(request, response);
-		if (subAction == null) throw new IllegalArgumentException("Invalid argument: null subAction");
-		if (!subAction.equals("miss") && !subAction.equals("complete")) throw new IllegalArgumentException("Invalid argument: subAction must be 'complete' or 'miss'");
-		
-		this.subAction = subAction;
 	}
 
-	@Override
-	public void RunAction() {
+	public void runAction() {
+		throw new NotAcceptableException("Use RunAction(int) with sub-action as int value.");
+	}
+
+	public void runAction(final int newState) {
+
+		// Ensure valid action
+		if (newState != Appointment.STATE_COMPLETED && newState != Appointment.STATE_MISSED && newState != Appointment.STATE_MISSED_REFUNDED) {
+			throw new IllegalArgumentException("Invalid argument: subAction must be 'complete' or 'miss' or 'refund'");
+		}
 
 		// This should not be possible for anyone not logged in.
 		if (uid <= 0) {
@@ -26,41 +30,71 @@ public class MissOrCompleteAppointmentAction extends ActionRunner {
 			return;
 		}
 
+		// Check for a valid appointment ID
 		final String appointmentIdString = QueryHelpers.getPost(request, "appointmentId");
 		int appointmentIdInt;
 		try {
 			appointmentIdInt = Integer.parseInt(appointmentIdString);
 		} catch (NumberFormatException e) {
-			appointmentIdInt = 0;
+			this.sendPostReply("/appointments", "", "Not a valid appointment ID.");
+			return;
 		}
-		final Appointment appointment = DataManager.getAppointmentDAO().retrieveByID(appointmentIdInt);
+
+		// Verify connection to DB
+		final DAO<Appointment> appointmentDAO = DataManager.getAppointmentDAO();
+		if (appointmentDAO == null) {
+			this.sendPostReply("/appointments", "", "Couldn't connect to database, please try again.");
+			return;
+		}
+
+		// Verify appointment exists
+		final Appointment appointment = appointmentDAO.retrieveByID(appointmentIdInt);
 		if (appointment == null) {
 			this.sendPostReply("/appointments", "", "Appointment " + appointmentIdString + " not found!");
 			return;
 		}
 
-		// Must be in past
+		// Appointment must be in past
 		if (!DateHelpers.isInThePast(appointment.getEndTime().toLocalDateTime())) {
-			this.sendPostReply("/appointments", "", "Cannot " + subAction + " appointments that haven't happened yet.");
+			this.sendPostReply("/appointments", "", "Cannot update state of appointments that haven't happened yet.");
 			return;
 		}
 
-		// Make sure is admin or instructor
-		if (!isAdmin && !isInstructor) {
-			this.sendPostReply("/appointments", "", "Appointment can only be marked as completed or missed by an admin or an instructor.");
+		// Make sure is admin or instructor modifying own class
+		if (!isAdmin && !(isInstructor && appointment.getIsMyAppointment(uid))) {
+			this.sendPostReply("/appointments", "", "Appointment state can only be modified by admin or instructor of class.");
 			return;
 		}
 
-		// Make sure can write to others (admin) or write to own (teachers)
-		if (!isAdmin && !appointment.getIsMyAppointment(uid)) {
-			this.sendPostReply("/appointments", "", "Cannot " + subAction + " others' appointments.");
-			return;
+		// IF IS A REFUND
+		if (newState == Appointment.STATE_MISSED_REFUNDED) {
+			
+			// Must be marked as missed
+			if (appointment.getWasCompleted()) {
+				this.sendPostReply("/appointments", "", "Cannot refund appointments that weren't missed.");
+				return;
+			}
+	
+			// Must not be marked as already refunded if refunding
+			if (appointment.getWasRefunded()) {
+				this.sendPostReply("/appointments", "", "Cannot double-refund appointments that were already refunded.");
+				return;
+			}
+
+		} else {
+
+			// Cannot update state of appointment that was refunded
+			// Note: This is here because I want a different error message than above
+			if (appointment.getWasRefunded()) {
+				this.sendPostReply("/appointments", "", "Cannot update appointment state for appointments that were refunded.");
+				return;
+			}
+
 		}
 
-		logger.debug("Attempting to mark appointment " + appointment.getRecID() + " as '" + subAction + "' ...");
-		
-		appointment.setCompletionState(subAction);
-		logger.debug("Deleted appointment ID: [{}]", appointmentIdString);
+		logger.debug("Attempting to set appointment [{}] state to [{}] ...", appointmentIdString, newState);
+		appointment.setCompletionState(newState);
+		logger.debug("Updated appointment ID: [{}]", appointmentIdString);
 		
 		this.sendPostReply("/appointments", "", "Appointment " + appointmentIdString + " updated!");
 		return;
