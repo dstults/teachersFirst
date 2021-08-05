c6b46c9740bebcd100a6bdb482d8f5ad667bb938
DranKof <drankof@gmail.com>
2021-08-05 06:22:18 -0700

    DarrenS: Persistent message console!

--- src/main/webapp/scripts/messageConsole.js
+++ src/main/webapp/scripts/messageConsole.js
@@ -25,17 +25,27 @@ const getDateTimeStamp = _ => {
 	return `${mo}/${d} ${h}:${m}`;
 };
 
+const writeMessageToConsole = (logMessage, animate) => {
+	if (consoleMessageList.style.display === 'none') consoleMessageList.style.display = 'block';
+	const newLine = document.createElement('li');
+	newLine.innerText = logMessage;
+	consoleMessageList.appendChild(newLine);
+	consoleMessageList.scrollTop = consoleMessageList.scrollHeight;
+	if (animate) {
+		setTimeout(_ => newLine.style.animation = 'none', 1000);
+	} else {
+		newLine.style.animation = 'none';
+	}
+};
+
 const addMessage = (text) => {
 	if (!text) return;
 	
-	if (consoleMessageList.style.display === 'none') consoleMessageList.style.display = 'block';
-	
-	const newLine = document.createElement('li');
-	newLine.innerText = getDateTimeStamp() + ' - ' + text;
-	consoleMessageList.appendChild(newLine);
+	const timestamp = getDateTimeStamp();
+	const logMessage = timestamp + ' - ' + text;
+	persistentLog.push(logMessage);
 
-	consoleMessageList.scrollTop = consoleMessageList.scrollHeight;
-	setTimeout(_ => newLine.style.animation = 'none', 1000);
+	writeMessageToConsole(logMessage, true);
 };
 
 const addError = (text) => {
@@ -43,4 +53,35 @@ const addError = (text) => {
 
 	addMessage('ERROR: ' + text);
 	console.error(text);
+};
+
+// Persistent logging between pages:
+
+const persistentLog = sessionStorage.getItem('persistentLog') ? JSON.parse(sessionStorage.getItem('persistentLog')) : [];
+
+const relogMessages = _ => {
+	// Re-populate past log messages without animating
+	for(const logMessage of persistentLog) {
+		writeMessageToConsole(logMessage, false);
+	}
+
+	// Re-hide if it was shown despite having been minimized before
+	const consoleVisible = consoleMessageList.style.display !== 'none';
+	const consolePreviouslyVisible = sessionStorage.getItem('consoleVisible') === 'true';
+	if (consoleVisible !== consolePreviouslyVisible) toggleVisiblity();
+
+	// Move console to wherever you had it
+	const consoleLocation = sessionStorage.getItem('consoleLocation') ? JSON.parse(sessionStorage.getItem('consoleLocation')) : {x: consoleFrame.style.left, y: consoleFrame.style.top};
+	consoleFrame.style.left = consoleLocation.x;
+	consoleFrame.style.top = consoleLocation.y;
+};
+relogMessages();
+
+// Make it save to session when leaving the current window
+window.onbeforeunload = _ => {
+	const maxStoredLines = 20;
+	const storedLog = persistentLog.splice(persistentLog.length > maxStoredLines ? persistentLog.length - maxStoredLines : 0);
+	sessionStorage.setItem('persistentLog', JSON.stringify(storedLog));
+	sessionStorage.setItem('consoleVisible', consoleMessageList.style.display !== 'none');
+	sessionStorage.setItem('consoleLocation', JSON.stringify({x: consoleFrame.style.left, y: consoleFrame.style.top}));
 };
