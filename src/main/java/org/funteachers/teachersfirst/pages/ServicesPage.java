edf1e3f97456cabd13b5c33538dae35e727d6cdd
Darren Stults <drankof@gmail.com>
2021-08-18 14:53:08 -0700

    DarrenS: Remove service POJO + DAO (#147)

--- src/main/java/org/funteachers/teachersfirst/pages/ServicesPage.java
+++ src/main/java/org/funteachers/teachersfirst/pages/ServicesPage.java
@@ -1,11 +1,8 @@
 package org.funteachers.teachersfirst.pages;
 
-import java.util.*;
 import javax.servlet.http.*;
 
 import org.funteachers.teachersfirst.*;
-import org.funteachers.teachersfirst.daos.DAO;
-import org.funteachers.teachersfirst.obj.*;
 
 public class ServicesPage extends PageLoader {
 
@@ -22,37 +19,8 @@ public class ServicesPage extends PageLoader {
 	@Override
 	public void loadPage() {
 		templateDataMap.put("title", "Services");
-		
-		// Check DAO connection
-		final DAO<Service> serviceDAO = DataManager.getServiceDAO();
-		if (serviceDAO == null) {
-			// Failed to contact SQL Server
-			templateName = "messageOnly.ftl";
-			templateDataMap.put("message", "Failed to connect with database, please try again.");
-			trySendResponse();
-			DataManager.resetDAOs();
-			return;
-		}
-
-		// Get data from DAO
-		final List<Service> services = serviceDAO.retrieveAll();
-
-		// Go
-		if (jsonMode) {
-			String json = JsonUtils.BuildArrays(services);
-			//logger.debug("Json: " + json);
-			trySendJson(json);
-		} else {
-			// FreeMarker
-			if (services == null || services.size() == 0) {
-				templateName = "messageOnly.ftl";
-				templateDataMap.put("message", "Could not contact database or no services loaded.");
-			} else {
-				templateName = "services.ftl";
-				templateDataMap.put("services", services);
-			}
-			trySendResponse();
-		}
+		templateName = "services.ftl";
+		trySendResponse();
 	}
 
 }
