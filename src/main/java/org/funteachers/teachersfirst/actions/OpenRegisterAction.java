3ec33cba922a59ea91ee92ff68c28f03772bdc56
DranKof <drankof@gmail.com>
2021-09-01 07:12:43 -0700

    DarrenS: Fix member insert so that it works
    Leaves out security-related items from new member insert, calls proper security-related items post-insertion.
    Fixed new members being created as admin instead of as students.

--- src/main/java/org/funteachers/teachersfirst/actions/OpenRegisterAction.java
+++ src/main/java/org/funteachers/teachersfirst/actions/OpenRegisterAction.java
@@ -13,52 +13,53 @@ public class OpenRegisterAction extends ActionRunner {
 
 	@Override
 	public void runAction() {
-
-		// This version of this process requires that you're not signed in.
-		if (uid > 0) {
-			this.sendPostReply("/services", "", "Please sign out before trying to register a new account!");
+		final boolean registerSelf = this.uid <= 0;
+		final boolean canRegisterOther = this.isInstructor || this.isAdmin;
+		if (!registerSelf && !canRegisterOther) {
+			this.sendPostReply("/logout", "", "Please sign out before trying to register a new account!");
 			return;
 		}
-		if (!DataManager.enableOpenRegistration) {
+		if (registerSelf && !DataManager.enableOpenRegistration) {
 			this.sendPostReply("/services", "", "Open registration is disabled!");
 			return;
 		}
 
-		String loginName = QueryHelpers.getPost(request, "loginName");
-		String password1 = QueryHelpers.getPost(request, "password1");
-		String password2 = QueryHelpers.getPost(request, "password2");
-		String displayName = QueryHelpers.getPost(request, "displayName");
+		final String retryPage = registerSelf ? "/register" : "/create_user";
+		final String loginName = QueryHelpers.getPost(request, "loginName");
+		final String password1 = QueryHelpers.getPost(request, "password1");
+		final String password2 = QueryHelpers.getPost(request, "password2");
+		final String displayName = QueryHelpers.getPost(request, "displayName");
 		String gender = QueryHelpers.getPost(request, "gender");
 		/* String birthYear = QueryHelpers.getPost(request, "b_year");
 		String birthMonth = QueryHelpers.getPost(request, "b_month");
 		String birthDay = QueryHelpers.getPost(request, "b_day"); */
-		String phone1 = QueryHelpers.getPost(request, "phone1");
-		String phone2 = QueryHelpers.getPost(request, "phone2");
-		String email = QueryHelpers.getPost(request, "email");
+		final String phone1 = QueryHelpers.getPost(request, "phone1");
+		final String phone2 = QueryHelpers.getPost(request, "phone2");
+		final String email = QueryHelpers.getPost(request, "email");
 
 		final String retryString = "loginName=" + loginName + "&displayName=" + displayName + "&gender=" + gender + "&phone1=" + phone1 + "&phone2=" + phone2 + "&email=" + email;
 
-		//TODO: Must check to make sure string input does not exceed database lengths
+		// TODO: Should check clientside to make sure string input does not exceed database lengths
 		if (loginName.isEmpty()) {
-			this.sendPostReply("/register", retryString, "Please provide a valid login name.");
+			this.sendPostReply(retryPage, retryString, "Please provide a valid login name.");
 			return;
 		}
 		if (password1.isEmpty()) {
-			this.sendPostReply("/register", retryString, "Please provide a valid password.");
+			this.sendPostReply(retryPage, retryString, "Please provide a valid password.");
 			return;
 		}
 		if (displayName.isEmpty()) {
-			this.sendPostReply("/register", retryString, "Please provide a valid display name.");
+			this.sendPostReply(retryPage, retryString, "Please provide a valid display name.");
 			return;
 		}
 		if (password2.isEmpty() || !password2.equals(password1)) {
-			this.sendPostReply("/register", retryString, "Passwords do not match!");
+			this.sendPostReply(retryPage, retryString, "Passwords do not match!");
 			return;
 		}
 		// trim and lcase gender string -- if it's not empty, which is valid
 		if (gender.length() > 0) gender = gender.toLowerCase().substring(0, 1);
 		if (!gender.equals("m") && !gender.equals("f") && !gender.equals("")) {
-			this.sendPostReply("/register", retryString, "Please provide a valid gender (m/f/blank).");
+			this.sendPostReply(retryPage, retryString, "Please provide a valid gender (m/f/blank).");
 			return;
 		}
 
@@ -68,27 +69,31 @@ public class OpenRegisterAction extends ActionRunner {
 		List<Member> members = this.connectionPackage.getMemberDAO().retrieveAll();
 		for (Member member : members) {
 			if (member.getLoginName() == loginName) {
-				this.sendPostReply("/register", retryString, "Login name '" + loginName + "' already taken, please try another.");
+				this.sendPostReply(retryPage, retryString, "Login name '" + loginName + "' already taken, please try another.");
 				return;
 			}
 		}
 
-		//TODO: Birthdates
+		// TODO: Birthdates
 
-		MemberSqlDAO memberDAO = (MemberSqlDAO) this.connectionPackage.getMemberDAO();
-		//Member member = new Member(loginName, password1, displayName, birthdate, gender, "", phone1, phone2, email, true, false, false);
-		Member member = new Member(loginName, displayName, 0, gender, "", "", phone1, phone2, email, true, false, false);
-		int newMemberRecordId = memberDAO.insert(member);
+		final MemberSqlDAO memberDAO = (MemberSqlDAO) this.connectionPackage.getMemberDAO();
+		final Member member = new Member(loginName, displayName, 0, gender, "", "", phone1, phone2, email, false, false, true);
+		final int newMemberRecordId = memberDAO.insert(member);
 		member.setRecID(newMemberRecordId);
 		logger.info(memberDAO.size() + " records total");
 		logger.debug("Registered new member: [{}]", member);
 
 		// Add password hash to database
 		memberDAO.updatePassword(member, password1);
-		
-		// Log new user in, giving token cookie
-		security.login(member);
-		this.sendPostReply("/appointments", "", "Welcome new user!");
+
+		// Finalize as self-register or reigster-other
+		if (registerSelf) {
+			// Log new user in, giving token cookie
+			security.login(member);
+			this.sendPostReply("/appointments", "", "Welcome new user!");
+		} else {
+			this.sendPostReply("/members", "", "New user (" + member.getDisplayName() + ") registered with ID: " + newMemberRecordId);
+		}
 		return;
 	}
 	
