94dfe6fd5188d14c684ae04369a84179ff74b45c
DranKof <drankof@gmail.com>
2021-09-01 08:06:40 -0700

    DarrenS: Added FreeMarker configurations to FreeMarker initializer to make it not leak in production.

--- src/main/java/org/funteachers/teachersfirst/managers/FreeMarkerSetup.java
+++ src/main/java/org/funteachers/teachersfirst/managers/FreeMarkerSetup.java
@@ -13,6 +13,7 @@ public class FreeMarkerSetup {
 	final private static Logger logger = LogManager.getLogger();
 
 	final public static Configuration freeMarkerConfig = new Configuration(Configuration.VERSION_2_3_24);
+	
 	public static void initializeFreeMarker(String resourcesDir) throws ServletException {
 		if (resourcesDir == null) {
 			logger.warn("===========DEBUG HELP===========");
@@ -20,6 +21,8 @@ public class FreeMarkerSetup {
 			logger.warn("FreeMarker is attempting to initialize without a resourcesDir, this should not be possible.");
 			throw new RuntimeException("FreeMarker is attempting to initialize without a resourcesDir.");
 		}
+
+		// Set the template directory
 		String templateDir = resourcesDir + "/templates";
 		try {
 			freeMarkerConfig.setDirectoryForTemplateLoading(new File(templateDir));
@@ -28,6 +31,15 @@ public class FreeMarkerSetup {
 			logger.fatal(msg, e);
 			throw new UnavailableException(msg);
 		}
+
+		// Make FreeMarker not output its errors to user:
+		freeMarkerConfig.setTemplateExceptionHandler(TemplateExceptionHandler.IGNORE_HANDLER);
+		
+		// Set this to "true" (default) to log errors; set to "false" to hide them from the catalina log:
+		freeMarkerConfig.setLogTemplateExceptions(true);
+
+		//freeMarkerConfig.setTemplateExceptionHandler(new FreeMarkerExceptionHandler());
+		
 	}
 
 }
