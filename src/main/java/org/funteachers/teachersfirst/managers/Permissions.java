b989d2f0bb9d815baee6ad75cf663d402be59390
DranKof <drankof@gmail.com>
2021-09-06 23:06:46 -0700

    DarrenS: Regressed to originally correct delete opening logic

--- src/main/java/org/funteachers/teachersfirst/managers/Permissions.java
+++ src/main/java/org/funteachers/teachersfirst/managers/Permissions.java
@@ -12,19 +12,16 @@ public class Permissions {
 		if (!actor.getIsAdmin() && !actor.getIsInstructor()) return false;
 
 		// Non-admin cannot delete admin
-		if (target.getIsAdmin() && !actor.getIsAdmin()) return false;
+		if (!actor.getIsAdmin() && target.getIsAdmin()) return false;
 
 		return true;
 	}
 
 	public static boolean MemberCanDeleteOpening(Member operator, Opening opening) {
-		// All admins can delete all openings -- short out
+		// All admins can delete all openings
 		if (operator.getIsAdmin()) return true;
 
-		// Must at least be an instructor if not an admin (above)
-		if (!operator.getIsInstructor()) return false;
-
-		// If instructor, must be self
+		// Must be "owner" (opening's "instructor")
 		if (operator.getRecID() != opening.getInstructorID()) return false;
 
 		return true;
