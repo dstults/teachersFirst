4cdba648f8621ef1fdf5719e142a91db6012a6aa
DranKof <drankof@gmail.com>
2021-09-06 23:02:32 -0700

    DarrenS: Added delete opening to permissions

--- src/main/java/org/funteachers/teachersfirst/managers/Permissions.java
+++ src/main/java/org/funteachers/teachersfirst/managers/Permissions.java
@@ -1,6 +1,6 @@
 package org.funteachers.teachersfirst.managers;
 
-import org.funteachers.teachersfirst.obj.Member;
+import org.funteachers.teachersfirst.obj.*;
 
 public class Permissions {
 	
@@ -13,6 +13,19 @@ public class Permissions {
 
 		// Non-admin cannot delete admin
 		if (target.getIsAdmin() && !actor.getIsAdmin()) return false;
+
+		return true;
+	}
+
+	public static boolean MemberCanDeleteOpening(Member operator, Opening opening) {
+		// All admins can delete all openings -- short out
+		if (operator.getIsAdmin()) return true;
+
+		// Must at least be an instructor if not an admin (above)
+		if (!operator.getIsInstructor()) return false;
+
+		// If instructor, must be self
+		if (operator.getRecID() != opening.getInstructorID()) return false;
 
 		return true;
 	}
