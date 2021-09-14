a7e6aefd3a97f0d18179db5d9e1eee800cf3a4e6
DranKof <drankof@gmail.com>
2021-09-13 17:18:20 -0700

    DarrenS: Explicit schema, sample superuser, Member defaults

--- db.sql
+++ db.sql
@@ -8,20 +8,22 @@ CREATE USER 'teachersFirst'@'%' IDENTIFIED BY 'abcDEF123';
 GRANT ALL PRIVILEGES ON *.* to 'teachersFirst'@'%' WITH GRANT OPTION;
 
 
-CREATE TABLE members (
+-- Tables:
+
+CREATE TABLE teachersFirst.members (
     recID               INT(11)           NOT NULL AUTO_INCREMENT,
     loginName           VARCHAR(40)       NOT NULL,
     passwordHash        CHAR(40)          DEFAULT NULL,
     token               CHAR(40)          DEFAULT NULL,
     displayName         VARCHAR(60)       NOT NULL,
-    credits             FLOAT             NOT NULL,
-    birthdate           DATE              NOT NULL,
-    gender              VARCHAR(1)        NOT NULL,
-    selfIntroduction    VARCHAR(800)      NOT NULL,
-    instructorNotes     VARCHAR(800)      NOT NULL,
-    phone1              VARCHAR(20)       NOT NULL,
-    phone2              VARCHAR(20)       NOT NULL,
-    email               VARCHAR(40)       NOT NULL,
+    credits             FLOAT             NOT NULL DEFAULT 0,
+    birthdate           DATE              NOT NULL DEFAULT '1800-01-01',
+    gender              VARCHAR(1)        NOT NULL DEFAULT '',
+    selfIntroduction    VARCHAR(800)      NOT NULL DEFAULT '',
+    instructorNotes     VARCHAR(800)      NOT NULL DEFAULT '',
+    phone1              VARCHAR(20)       NOT NULL DEFAULT '',
+    phone2              VARCHAR(20)       NOT NULL DEFAULT '',
+    email               VARCHAR(40)       NOT NULL DEFAULT '',
     isAdmin             TINYINT(1)        NOT NULL DEFAULT 0,
     isInstructor        TINYINT(1)        NOT NULL DEFAULT 0,
     isStudent           TINYINT(1)        NOT NULL DEFAULT 0,
@@ -31,7 +33,7 @@ CREATE TABLE members (
 );
 
 
-CREATE TABLE appointments (
+CREATE TABLE teachersFirst.appointments (
     recID               INT(11)           NOT NULL AUTO_INCREMENT,
     studentID           INT(11)           NOT NULL,
     instructorID        INT(11)           NOT NULL,
@@ -43,7 +45,7 @@ CREATE TABLE appointments (
 );
 
 
-CREATE TABLE openings (
+CREATE TABLE teachersFirst.openings (
     recID               INT(11)           NOT NULL AUTO_INCREMENT,
     instructorID        INT(11)           NOT NULL,
     startTime           DATETIME          NOT NULL,
@@ -52,7 +54,7 @@ CREATE TABLE openings (
 );
 
 
-CREATE TABLE loggedEvents (
+CREATE TABLE teachersFirst.loggedEvents (
     recID               INT(11)           NOT NULL AUTO_INCREMENT,
     operator            INT(11)           NOT NULL,
     date                DATETIME          NOT NULL,
@@ -64,10 +66,12 @@ CREATE TABLE loggedEvents (
 -- Useful Snippets:
 
 -- Create new user (Note: last three number 1-0-0 are student/instructor/admin):
-INSERT INTO members (loginName, passwordHash, displayName, credits, birthdate, gender, instructorNotes, phone1, email, isAdmin, isInstructor, isStudent) VALUES ('studentLogin', SHA1('password'), 'Student Name', 0, '1995-05-25', 'm', 'Notes about student.', '444-444-4444', 'abc@xyz.com', 0, 0, 1);
-INSERT INTO members (loginName, passwordHash, displayName, credits, birthdate, gender, instructorNotes, phone1, email, isAdmin, isInstructor, isStudent) VALUES ('instructorLogin', SHA1('password'), 'Instructor Name', 0, '1995-05-25', 'm', 'Instructor introduction.', '444-444-4444', 'abc@xyz.com', 0, 1, 0);
-INSERT INTO members (loginName, passwordHash, displayName, credits, birthdate, gender, instructorNotes, phone1, email, isAdmin, isInstructor, isStudent) VALUES ('adminLogin', SHA1('password'), 'Admin Name', 0, '1995-05-25', 'm', 'No description needed.', '444-444-4444', 'abc@xyz.com', 1, 0, 0);
+INSERT INTO teachersFirst.members (loginName, passwordHash, displayName, birthdate, instructorNotes, isAdmin, isInstructor, isStudent) VALUES ('superuser', SHA1('password'), 'Superuser', '1800-01-01', 'This user has authority to perform administrative actions and cannot be deleted.', 1, 0, 0);
+
+INSERT INTO teachersFirst.members (loginName, passwordHash, displayName, credits, birthdate, gender, phone1, email, isAdmin, isInstructor, isStudent) VALUES ('adminLogin', SHA1('password'), 'Admin Name', 0, '1995-08-12', 'm', '444-444-4444', 'abc@xyz.com', 1, 0, 0);
+INSERT INTO teachersFirst.members (loginName, passwordHash, displayName, credits, birthdate, gender, selfIntroduction, phone1, email, isAdmin, isInstructor, isStudent) VALUES ('instructorLogin', SHA1('password'), 'Instructor Name', 0, '1985-03-24', 'm', 'Instructor introduction.', '444-444-4444', 'abc@xyz.com', 0, 1, 0);
+INSERT INTO teachersFirst.members (loginName, passwordHash, displayName, credits, birthdate, gender, instructorNotes, phone1, email, isAdmin, isInstructor, isStudent) VALUES ('studentLogin', SHA1('password'), 'Student Name', 0, '1990-05-08', 'm', 'Notes about student.', '444-444-4444', 'abc@xyz.com', 0, 0, 1);
 
 -- Change user password:
-UPDATE members SET passwordHash = SHA1('password') WHERE (id = 1);
+UPDATE teachersFirst.members SET passwordHash = SHA1('password') WHERE (id = 1);
 
