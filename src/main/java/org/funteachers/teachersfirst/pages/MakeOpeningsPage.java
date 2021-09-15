70274b1633cabf358cca55d542b1ab53a1bbd800
DranKof <drankof@gmail.com>
2021-09-15 13:36:25 -0700

    DarrenS: Thought about it, no point, should all be client side

--- src/main/java/org/funteachers/teachersfirst/pages/MakeOpeningsPage.java
+++ src/main/java/org/funteachers/teachersfirst/pages/MakeOpeningsPage.java
@@ -14,25 +14,6 @@ public class MakeOpeningsPage extends PageLoader {
 	public void loadPage() {
 		templateDataMap.put("title", "Create Openings");
 
-		// Days of Week - Data & FreeMarker
-		String daysOfWeekString = QueryHelpers.getGet(request, "daysOfWeek"); // SuMoTuWdThFrSa
-		if (daysOfWeekString.isEmpty()) daysOfWeekString = DateHelpers.getDayOfWeekStringFromToday();
-		final String sundayChecked = daysOfWeekString.contains("su") ? " checked" : "";
-		final String mondayChecked = daysOfWeekString.contains("mo") ? " checked" : "";
-		final String tuesdayChecked = daysOfWeekString.contains("tu") ? " checked" : "";
-		final String wednesdayChecked = daysOfWeekString.contains("we") ? " checked" : "";
-		final String thursdayChecked = daysOfWeekString.contains("th") ? " checked" : "";
-		final String fridayChecked = daysOfWeekString.contains("fr") ? " checked" : "";
-		final String saturdayChecked = daysOfWeekString.contains("sa") ? " checked" : "";
-		templateDataMap.put("daysOfWeek", daysOfWeekString);
-		templateDataMap.put("sundayChecked", sundayChecked);
-		templateDataMap.put("mondayChecked", mondayChecked);
-		templateDataMap.put("tuesdayChecked", tuesdayChecked);
-		templateDataMap.put("wednesdayChecked", wednesdayChecked);
-		templateDataMap.put("thursdayChecked", thursdayChecked);
-		templateDataMap.put("fridayChecked", fridayChecked);
-		templateDataMap.put("saturdayChecked", saturdayChecked);
-
 		// FreeMarker Template
 		templateName = "newOpenings.ftl";
 
