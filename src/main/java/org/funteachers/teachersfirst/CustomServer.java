1627235825d1ce5ab0b93ac5a561c26af00ff427
DranKof <drankof@gmail.com>
2021-09-16 09:47:51 -0700

    DarrenS: Isolate and serve profile images IAW permissions

--- src/main/java/org/funteachers/teachersfirst/CustomServer.java
+++ src/main/java/org/funteachers/teachersfirst/CustomServer.java
@@ -18,6 +18,8 @@ public class CustomServer {
 	
 	final protected static Logger logger = LogManager.getLogger();
 	final private static String customServeBasePath = "/var/www/";
+	final private static String internalDefaultProfilePath = System.getProperty("user.dir") + "/webapps/ROOT/images/profileNeutral.png";
+	final private static String customDefaultProfilePath = customServeBasePath + "images/profileNeutral.png";
 
 	final protected ConnectionPackage connectionPackage;
 	final protected HttpServletRequest request;
@@ -25,11 +27,6 @@ public class CustomServer {
 	final protected SecurityChecker security;
 
 	final protected Member operator;
-	final protected int uid;
-	final protected String userName;
-	final protected boolean isAdmin;
-	final protected boolean isInstructor;
-	final protected boolean isStudent;
 
 	protected String templateName = null;
 	protected String errorMessage = "";
@@ -42,29 +39,42 @@ public class CustomServer {
 		this.request = cp.getRequest();
 		this.response = cp.getResponse();
 		this.security = cp.getSecurity();
-
 		this.operator = security.getMemberFromRequestCookieToken();
-		if (operator != null) {
-			this.uid = this.operator.getRecID();
-			this.userName = this.operator.getDisplayName();
-			this.isAdmin = this.operator.getIsAdmin();
-			this.isInstructor = this.operator.getIsInstructor();
-			this.isStudent = this.operator.getIsStudent();
-		} else {
-			this.uid = 0;
-			this.userName = "Guest";
-			this.isAdmin = false;
-			this.isInstructor = false;
-			this.isStudent = false;
-		}
-
 	}
 
 	public void serve(String pagePath, String sanitizedQuery, ServletContext servletContext) throws IOException {
 
+		// Check to see if it's a profile image
+		final String regex = "^/custom/profiles/u([0-9]+).*$";
+		final Pattern pattern = Pattern.compile(regex);
+		final Matcher matcher = pattern.matcher(pagePath);
+		final boolean isProfilePic = matcher.matches();
+		boolean showRealProfilePic = false;
+		if (isProfilePic) {
+			int otherId = Integer.parseInt(matcher.group(1));
+			Member other = this.connectionPackage.getMemberDAO().retrieveByID(otherId);
+			if (Permissions.MemberCanSeeMember(this.operator, other)) {
+				showRealProfilePic = true;
+			}
+		}
+		
 		// Check to see if file exists
 		String filename = pagePath;
-		File file = new File(customServeBasePath, filename);
+		File file;
+		if (isProfilePic && showRealProfilePic) {
+			// Find the right profile pic file type by thrashing around
+			final String customProfileBasePath = "/custom/profiles/u" + matcher.group(1) + "/profile.";
+			file = new File(customServeBasePath, customProfileBasePath + "png");
+			if (!file.exists()) file = new File(customServeBasePath, customProfileBasePath + "gif");
+			if (!file.exists()) file = new File(customServeBasePath, customProfileBasePath + "jpg");
+			if (!file.exists()) file = new File(customDefaultProfilePath);
+			if (!file.exists()) file = new File(internalDefaultProfilePath);
+		} else if (isProfilePic && !showRealProfilePic) {
+			file = new File(customDefaultProfilePath);
+			if (!file.exists()) file = new File(internalDefaultProfilePath);
+		} else {
+			file = new File(customServeBasePath, filename);
+		}
 		if (!file.exists() || file.isDirectory()) {
 			logger.debug("====================== CUSTOM FILE NOT FOUND ======================");
 			logger.debug("Is Directory:     {}", file.isDirectory());
@@ -76,24 +86,6 @@ public class CustomServer {
 			return; // Use above logging instead of standard logging
 		}
 
-		// Check to see if it's a profile image
-		final String regex = "^/custom/profiles/u([0-9]+)/.*$";
-		final Pattern pattern = Pattern.compile(regex);
-		final Matcher matcher = pattern.matcher(pagePath);
-		logger.debug("Matcher matches: {}", matcher.matches());
-		logger.debug("Matcher groups: {}", matcher.groupCount());
-		logger.debug("Matcher group {}: {}", 0, matcher.group(0));
-		logger.debug("Matcher group {}: {}", 1, matcher.group(1));
-		if (matcher.matches()) {
-			int otherId = Integer.parseInt(matcher.group(1));
-			Member other = this.connectionPackage.getMemberDAO().retrieveByID(otherId);
-			if (!Permissions.MemberCanSeeMember(this.operator, other)) {
-				// Send fake file not found when not authorized to view profile pic
-				response.sendError(HttpServletResponse.SC_NOT_FOUND);
-				return;
-			}
-		}
-		
 		// Send file -- set headers and send raw data
 		this.response.setHeader("Content-Type", servletContext.getMimeType(filename));
 		this.response.setHeader("Content-Length", String.valueOf(file.length()));
