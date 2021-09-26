ccd4d1fdc1607135364e012c55c49f26fc7532c0
DranKof <drankof@gmail.com>
2021-09-25 19:19:28 -0700

    DarrenS: Added between dates prettified appointment hybrid

--- src/main/java/org/funteachers/teachersfirst/daos/sql/HybridDAO.java
+++ src/main/java/org/funteachers/teachersfirst/daos/sql/HybridDAO.java
@@ -20,7 +20,7 @@ public class HybridDAO {
 
 		this.conn = conn;
 	}
-
+	
 	public List<Appointment> getAppointmentsWithMemberNames() {
 		logger.debug("Appointments SELECT [ * ] ...");
 		
@@ -44,7 +44,34 @@ public class HybridDAO {
 		return appointments;
 	}
 
-	
+	public List<Appointment> getAppointmentsWithMemberNamesBetweenDatetimeAndDatetime(LocalDateTime start, LocalDateTime end) {
+		final String startStringSql = DateHelpers.toSqlDatetimeString(start);
+		final String endStringSql = DateHelpers.toSqlDatetimeString(end);
+		logger.debug("Appointments SELECT [ * ] ...");
+		
+		String query = "SELECT a.recID, a.studentID, m1.displayName AS 'studentName', a.instructorID, m2.displayName AS 'instructorName', " +
+						"a.startTime, a.endTime, a.schedulingVerified, a.completionState " +
+						"FROM appointments a " +
+						"LEFT JOIN members m1 ON a.studentID = m1.recID " +
+						"LEFT JOIN members m2 ON a.instructorID = m2.recID " +
+						"WHERE startTime >= ? AND endTime <= ? " +
+						"ORDER BY startTime, instructorID, endTime;";
+
+		List<SQLRow> rows = SQLUtils.executeSql(conn, query, startStringSql, endStringSql);
+		if (rows == null || rows.size() == 0) {
+			logger.debug("No appointments found!");
+			return null;
+		}
+
+		List<Appointment> appointments = new ArrayList<>();
+		for (SQLRow row : rows) {
+			Appointment appointment = convertRowToPrettifiedAppointment(row);
+			appointments.add(appointment);
+		}
+		return appointments;
+	}
+
+			
 	private Appointment convertRowToPrettifiedAppointment(SQLRow row) {
 		//logger.debug("Converting " + row + " to Appointment...");
 		int recID = Integer.parseInt(row.getItem("recID"));
